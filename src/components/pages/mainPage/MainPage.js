d8314843a9eeb0d404d4a3a7e3ab17f41dcea604
Nazar <zvarych.nz@gmail.com>
2023-04-19 19:43:22 +0300

    add react slick and did carousel

--- src/components/pages/mainPage/MainPage.js
+++ src/components/pages/mainPage/MainPage.js
@@ -1,15 +1,21 @@
 import Header from "../../header/Header";
 import CardList from "../../cardList/CardList";
+import Slider from "react-slick";
 
 import search from "../../../resources/images/search-icon.svg"
+import leftArrow from "../../../resources/images/arrow-left.svg"
+import slideImg from "../../../resources/images/slider/Slide.png"
 
+import "./slick.scss"; 
+import "./slick-theme.scss";
 import "./mainPage.scss"
 
 const MainPage = () => {
 
-    return (
+    return ( 
         <>
             <Header/>
+            <MainSlider/>
             <div className="subHeader">
                 <h2 className="subHeader__title">All sneakers</h2>
                 <div className="search_panel">
@@ -19,6 +25,53 @@ const MainPage = () => {
             </div>
             <CardList/>
         </>
+    )
+}
+
+const LeftArrow = ({onClick, className, style}) => {
+    return (
+        <div
+            className={className}
+            onClick={onClick}>
+            <img src={leftArrow} alt="arrow"/>
+        </div>
+    )
+}
+
+const RigthArrow = ({onClick, className, style}) => {
+    return (
+        <div
+            className={className}
+            onClick={onClick}>
+                <img src={leftArrow} alt="arrow" style={{transform: "translate(-50%, -50%) rotate(180deg)"}}/>
+        </div>
+    )
+}
+
+const MainSlider = () => {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <RigthArrow />,
+        prevArrow: <LeftArrow />
+    };
+
+    return (
+        <div >
+            <Slider {...settings}>
+                <div className="slide">
+                    <img src={slideImg} alt="slide"/>
+                </div>
+                <div className="slide">
+                    <img src={slideImg} alt="slide"/>
+                </div>
+                <div className="slide">
+                    <img src={slideImg} alt="slide"/>
+                </div>
+            </Slider>
+        </div>
     )
 }
 
